107ed2816bab27d993cd0d4cecf8ed14aeb56156
Remilia <HinanawiCirno@outlook.com>
2017-07-10 14:45:49 +0800

    add Kafka Project

---
+++ Nereus-Kafka/src/main/java/me/mushen/nereus/kafka/config/KafkaConfig.java
@@ -0,0 +1,15 @@
+package me.mushen.nereus.kafka.config;
+
+import java.util.Properties;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2017-07-10
+ */
+public class KafkaConfig {
+
+    private static Properties producerProps() {
+
+    }
+}
---
+++ Nereus-Kafka/src/main/java/me/mushen/nereus/kafka/producer/MyProducer.java
@@ -0,0 +1,13 @@
+package me.mushen.nereus.kafka.producer;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2017-07-10
+ */
+public class MyProducer {
+
+    public static void main(String[] args){
+
+    }
+}
